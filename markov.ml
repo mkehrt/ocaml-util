aa37a7998a631f9127b8456a7151c4806767ffaa
Matthew Kehrt <mkehrt@roseandsigil.net>
2014-03-01 17:01:18 -0800

    Some util, zipper work in support of counting

--- markov.ml
+++ markov.ml
@@ -4,6 +4,14 @@
 module Util : sig
   module List : sig
     val flatten : 'a option list -> 'a list 
+    (* val find : ('a -> bool) -> 'a list -> 'a option *)
+    module Zipper : sig
+      type 'a t
+      val unzip : 'a list -> 'a -> 'a t
+      val modify : ('a -> 'a) -> 'a  t -> 'a t
+      val map : ('a -> 'b) -> 'a t -> 'b t
+      val zip : 'a t -> 'a list
+    end
   end
   module Option : sig
     val getOrElse : 'a -> 'a option -> 'a
@@ -16,6 +24,25 @@ end = struct
     | []           -> []
     | (Some x)::tl -> x::(flatten tl) 
     | None:: tl    -> flatten tl
+
+    module Zipper = struct
+      type 'a t = 'a list * 'a * 'a list
+
+      let rec unzip l x = match l with
+        | [] -> (l,x,[])
+        | y::tl ->
+          if y = x
+          then ([], y, tl)
+          else let (l,z,r) = unzip tl x in (y::l, z, r)
+
+      let modify f (l,x,r) = (l, f x, r)
+
+      let map f (l, x, r) = (List.map f l, f x, List.map f r)
+
+      let rec zip (l,x,r) = l @ [x] @ r
+
+    end
+
   end
 
   module Option = struct
@@ -29,8 +56,8 @@ end = struct
   end
 end
 
-module UL = Util.List
-module UO = Util.Option
+module L = Util.List
+module O = Util.Option
 
 
 module Picker : sig
@@ -55,6 +82,9 @@ module Trie : sig
   type 'a t
   type 'a weight
   type 'a count
+
+  (* val countIterate: 'a list -> 'a count -> 'a count
+  val countFromList: 'a list list -> 'a count *)
   
   val normalize : 'a count -> 'a weight
   val pickPath : 'a weight -> 'a list
@@ -65,6 +95,12 @@ end = struct
 
   type 'a weight = ('a * float) t
   type 'a count = ('a * int) t
+
+  (* val countIterate l ct = match (l, ct) with
+    | ([], ct') -> ct'
+    | (x::tl, Leaf) -> Node [((x, 1), countIterate tl)]
+    | (x::tl, Node cts) -> (L.find (fun ((y, _), _) -> x = y) cts) *)
+    
 
   let rec normalize wted = match wted with
     | Leaf -> Leaf
@@ -80,7 +116,7 @@ end = struct
     | Node l ->
       begin
         let po = Picker.pick (List.map (fun ((x,wt), wted') -> ((x, wted'), wt)) l) in
-        UO.getOrElse [] (UO.map (fun (x, wted) -> x :: pickPath wted) po)
+        O.getOrElse [] (O.map (fun (x, wted) -> x :: pickPath wted) po)
       end
 end
 
